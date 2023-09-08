aa66a6977381c2acad2acc34c56e927c386acaff
shbinn <shbinn23@gmail.com>
2023-09-09 02:27:43 +0900

    9/9 인프런 스프링 기본 (스프링 컨테이너부터 시작)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,20 +1,37 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration  // 설정 정보 (의존성 주입, DI)
 public class AppConfig {    // 실제 동작에 필요한 구현 객체를 생성한다.
 
+    @Bean   // 스프링 컨테이너에 스프링 빈으로 등록
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository()); // 주입
+        return new MemberServiceImpl(memberRepository()); // 주입
     }
 
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy()); // 주입
+        return new OrderServiceImpl(memberRepository(), discountPolicy()); // 주입
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 }
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,7 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService{
 
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final MemberRepository memberRepository;    // 추상화에만 의존 (오직 인터페이스), 구체적인건 AppConfig 에서
+    private final MemberRepository memberRepository;    // 추상화에만 의존 (오직 인터페이스), 구체적인건 AppConfig 에서 객체 인스턴스의 참조값이 변수에 저장된
 
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
